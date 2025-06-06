e379cdd2dd1b25d55fa959acd201a9868359c0fc
Buse Nur Boz <104280765+busebz@users.noreply.github.com>
2025-06-06 19:12:37 +0300

    Page loading and cv updated

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
+const redirectedPath = window.location.search;
+if (redirectedPath.startsWith("?/")) {
+  const cleanPath = redirectedPath.slice(2);
+  window.history.replaceState(null, "", "/" + cleanPath);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
